ffaa5540920b32c05d53342aaba7f9785beee759
penyaev <ilya@penyaev.com>
2025-12-13 13:19:55 +0100

    day2

---
+++ src/bin/day02_1.rs
@@ -0,0 +1,78 @@
+use aoc_2025::{input_file, read_split};
+
+struct InvalidIDIterator {
+    to: u64,
+    current: u64,
+}
+
+fn num_len(x: u64) -> u8 {
+    let mut l: u8 = 1;
+    while x >= 10u64.pow(l as u32) {
+        l += 1;
+    }
+
+    l
+}
+
+impl Iterator for InvalidIDIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.current > self.to {
+                return None;
+            }
+
+            let len = num_len(self.current);
+            if len % 2 == 1 {
+                self.current = 10u64.pow(len as u32);
+                continue;
+            }
+
+            let half_pow = 10u64.pow((len as u32)/2);
+            let half = self.current / half_pow;
+            let num = half * half_pow + half;
+
+            if num < self.current {
+                self.current = (half + 1) * half_pow;
+                continue;
+            }
+            if num > self.to {
+                return None;
+            }
+
+            self.current = num + 1;
+            return Some(num);
+        }
+    }
+}
+fn invalid_ids(from: u64, to: u64) -> InvalidIDIterator {
+    InvalidIDIterator {
+        to,
+        current: from,
+    }
+}
+
+fn main() {
+    let input = read_split(input_file(2, false), b',').expect("failed to read input");
+
+    let mut result: u64 = 0;
+    for chunk_result in input {
+        let chunk = chunk_result.expect("failed to read chunk from input");
+        let s = String::from_utf8(chunk).expect("failed to parse chunk");
+
+        let parts: Vec<&str> = s.split('-').collect();
+        if parts.len() != 2 {
+            panic!("invalid input");
+        }
+        let from: u64 = parts[0].parse().expect("failed to parse from");
+        let to: u64 = parts[1].parse().expect("failed to parse to");
+
+        for invalid_id in invalid_ids(from, to) {
+            //println!("[{}; {}] {}", from, to, invalid_id);
+            result += invalid_id;
+        }
+    }
+
+    println!("{}", result)
+}
---
+++ src/bin/day02_2.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+use aoc_2025::{input_file, read_split};
+
+fn num_len(x: u64) -> u8 {
+    let mut l: u8 = 1;
+    while x >= 10u64.pow(l as u32) {
+        l += 1;
+    }
+
+    l
+}
+
+
+fn invalid_ids(from: u64, to: u64) -> Vec<u64> {
+    let mut result: Vec<u64> = Vec::new();
+    for len in num_len(from)..=num_len(to) {
+        let mut seen: HashSet<u64> = HashSet::new();
+        for prefix_len in 1..=len/2 {
+            if len % prefix_len != 0 {
+                continue;
+            }
+            let repeat = len / prefix_len;
+
+            for prefix in 10u64.pow((prefix_len-1) as u32)..10u64.pow(prefix_len as u32) {
+                let mut num = 0u64;
+                for _ in 0..repeat {
+                    num *= 10u64.pow(prefix_len as u32);
+                    num += prefix;
+                }
+
+                if num >= from && num <= to && seen.insert(num) {
+                    result.push(num);
+                }
+            }
+        }
+    }
+
+    result
+}
+
+fn main() {
+    let input = read_split(input_file(2, false), b',').expect("failed to read input");
+
+    let mut result: u64 = 0;
+    for chunk_result in input {
+        let chunk = chunk_result.expect("failed to read chunk from input");
+        let s = String::from_utf8(chunk).expect("failed to parse chunk");
+
+        let parts: Vec<&str> = s.split('-').collect();
+        if parts.len() != 2 {
+            panic!("invalid input");
+        }
+        let from: u64 = parts[0].parse().expect("failed to parse from");
+        let to: u64 = parts[1].parse().expect("failed to parse to");
+
+        for invalid_id in invalid_ids(from, to) {
+            // println!("[{}; {}] {}", from, to, invalid_id);
+            result += invalid_id;
+        }
+    }
+
+    println!("{}", result)
+}
